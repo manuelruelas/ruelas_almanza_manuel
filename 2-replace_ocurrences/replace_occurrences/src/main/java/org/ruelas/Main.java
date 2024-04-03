a374f55359154bbf034ff44f63df46fe445b810e
Manuel Ruelas <manuel.r.a@hotmail.com>
2024-04-02 23:18:39 -0700

    Create replace occurrences function

---
+++ 2-replace_ocurrences/replace_occurrences/src/main/java/org/ruelas/Main.java
@@ -0,0 +1,21 @@
+package org.ruelas;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayReplace(new int[]{1, 2, 1}, 1, 3)));
+    }
+
+    /// Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+    static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == elemToReplace) {
+                inputArray[i] = substitutionElem;
+            }
+        }
+        return inputArray;
+    }
+}
